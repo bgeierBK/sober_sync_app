a73b406ecd230b8afb632a60af9e0e82a3cbdaa2
charliekozey <kozey@mailbox.org>
2025-07-23 22:28:06 -0400

    Burger improvements

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -26,11 +26,12 @@ function NavBar({ currentUser, setCurrentUser }) {
         console.error("Problem with logout:", error.message);
         alert("Problem with logout:" + error.message);
       });
-    }
-  
-      function handleBurgerClick() {
-        setBurgerIsOpen(state => !state);
-      }
+      setBurgerIsOpen(state => false)
+  }
+
+  function handleBurgerClick() {
+    setBurgerIsOpen(state => !state);
+  }
 
   // Debug currentUser object - you can remove this after fixing the issue
   console.log("Current user in navbar:", currentUser);
@@ -48,11 +49,11 @@ function NavBar({ currentUser, setCurrentUser }) {
             SOBER SYNC
           </h1>
         </NavLink>
-        <NavLink 
-          role="button" 
-          className={burgerIsOpen ? "navbar-burger is-active" : "navbar-burger"} 
-          aria-label="menu" 
-          aria-expanded="false" 
+        <NavLink
+          role="button"
+          className={burgerIsOpen ? "navbar-burger is-active" : "navbar-burger"}
+          aria-label="menu"
+          aria-expanded="false"
           data-target="navbarBasicExample"
           onClick={handleBurgerClick}
         >
@@ -68,14 +69,24 @@ function NavBar({ currentUser, setCurrentUser }) {
         <div id="menu" className={burgerIsOpen ? "navbar-menu is-active" : "navbar-menu"} >
           <div className="navbar-end">
             <div className="navbar-item">
-              <div className="buttons">
-                <NavLink to="/signup" className="button is-primary">
-                  <strong>Sign up</strong>
-                </NavLink>
-                <NavLink to="/login" className="button is-light">
-                  Log in
-                </NavLink>
-              </div>
+              <NavLink to="/about" className="navbar-item" onClick={handleBurgerClick}>
+                About
+              </NavLink>
+            </div>
+            <div className="navbar-item">
+              <NavLink to="/" className="navbar-item" onClick={handleBurgerClick}>
+                Events
+              </NavLink>
+            </div>
+            <div className="navbar-item">
+              <NavLink to="/signup" className="button is-light" onClick={handleBurgerClick}>
+                Sign up
+              </NavLink>
+            </div>
+            <div className="navbar-item">
+              <NavLink to="/login" className="button is-light" onClick={handleBurgerClick}>
+                Log in
+              </NavLink>
             </div>
           </div>
         </div>
@@ -83,7 +94,7 @@ function NavBar({ currentUser, setCurrentUser }) {
 
         <div id="menu" className={burgerIsOpen ? "navbar-menu is-active" : "navbar-menu"} >
           <div className="navbar-start">
-            <NavLink to="/" className="navbar-item">
+            <NavLink to="/" className="navbar-item" onClick={handleBurgerClick}>
               Home
             </NavLink>
             <NavLink className="navbar-item"
@@ -91,13 +102,15 @@ function NavBar({ currentUser, setCurrentUser }) {
                 currentUser && currentUser.id
                   ? `/users/${currentUser.id}`
                   : "/profile"
-              }>
+              }
+              onClick={handleBurgerClick}
+            >
               My Profile
             </NavLink>
-            <NavLink to="/thelounge" className="navbar-item">
+            <NavLink to="/thelounge" className="navbar-item" onClick={handleBurgerClick}>
               The Lounge
             </NavLink>
-            <NavLink to="/about" className="navbar-item">
+            <NavLink to="/about" className="navbar-item"  onClick={handleBurgerClick}>
               About
             </NavLink>
           </div>
